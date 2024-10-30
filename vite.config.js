84a6f5d6f9fe7b2d16edc36eaffe2eba48a6bb64
fan <65284460@qq.com>
2024-10-30 20:37:05 +0800

    '1'

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),createHtmlPlugin({ template: 'src/index.html' })],
+	  build: {
+    outDir: '.', // 将生成的文件直接放在项目根目录
+    assetsDir: 'js' // 将 JS 文件放到根目录下的 js 子目录中
+  }
 })
